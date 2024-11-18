54c2303780810283c64593eb11b77e26a0f20af1
Arturo <artio9000@gmail.com>
2024-11-18 14:28:45 -0800

    More testing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,16 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
-
+import prettierConfig from "eslint-config-prettier";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  "extends", [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:prettier/recommended"
-    ],
-
-  
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    ...pluginJs.configs.recommended, // ESLint's recommended JavaScript rules
+    ...pluginReact.configs.flat.recommended, // React rules for flat config
+    ...prettierConfig, // Prettier rules for code formatting
+  },
 ];
-

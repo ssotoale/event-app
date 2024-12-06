a4f697e49340e6ae5184d61dbc6aa4b1fdda309c
ssoto <52551359+ssotoale@users.noreply.github.com>
2024-12-05 20:50:52 -0800

    Update createAccount.jsx
    force push process.env.API_PREFIX to connect frontend to cloud

--- packages/quest-frontend/src/createAccount.jsx
+++ packages/quest-frontend/src/createAccount.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 // Add your backend API URL
-const API_PREFIX = "http://localhost:5000";
+const API_PREFIX = process.env.API_PREFIX;
 
 const registerUser = (username, password, setMessage) => {
   return fetch(`${API_PREFIX}/api/create-account`, {
